3606f8214e98c863d875d28523a1a9d3f1f22af9
rohitmehtajuly31 <rohitmehtajuly31@gmail.com>
2024-05-10 19:11:20 +0530

    Flag image for all currency

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-cosnt Base_url="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json"
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const dropdowns = document.querySelectorAll('.dropdown select');
+
+for (let select of dropdowns) {
+    for (let currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        
+        if (currCode === "USD" && select.name === 'from') {
+            newOption.selected = "selected";
+        } else if (currCode === "INR" && select.name === 'to') {
+            newOption.selected = "selected";
+        }
+        
+        select.append(newOption);
+    }
+
+    select.addEventListener("change",(evt)=>{
+        updateflag(evt.target)
+    })
+}
+const updateflag=((element)=>{
+    let currcode=element.value;
+    let countrycode=countryList[currcode];
+    let newsrc=`https://flagsapi.com/${countrycode}/flat/64.png`
+    let img = element.parentElement.querySelector('img');
+    img.src=newsrc;
+})
